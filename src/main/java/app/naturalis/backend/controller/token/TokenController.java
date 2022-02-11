44cd62165967c8bad2cb37a6904fb050d7a4a1f8
jvrodrigs <joao.vitor@sltributos.com.br>
2022-02-11 16:52:22 -0300

    logout da app

---
+++ src/main/java/app/naturalis/backend/controller/token/TokenController.java
@@ -0,0 +1,27 @@
+package app.naturalis.backend.controller.token;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+
+    @DeleteMapping("/revoke")
+    public void revokeToken(HttpServletRequest req, HttpServletResponse res){
+        Cookie cookie = new Cookie("refreshToken", null);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath(req.getContextPath() + "/oauth/token");
+        cookie.setMaxAge(0);
+
+        res.addCookie(cookie);
+        res.setStatus(HttpStatus.NO_CONTENT.value());
+    }
+}
