733ffcdc4ae6b037a28e7585699e3c24f902598d
jvrodrigs <joao.vitor@sltributos.com.br>
2022-02-15 16:33:31 -0300

    criação de novos funcionarios, adicionando suas permissoes de acordo com o cargo

--- src/main/java/app/naturalis/backend/repository/FuncionarioRepository.java
+++ src/main/java/app/naturalis/backend/repository/FuncionarioRepository.java
@@ -2,7 +2,10 @@ package app.naturalis.backend.repository;
 
 import app.naturalis.backend.model.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -10,4 +13,9 @@ import java.util.Optional;
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
 
     public Optional<Funcionario> findByEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query(value = "insert into funcionario_perm (funcionario_id,permissao_id) VALUES (:idFunc,:idPerm)", nativeQuery = true)
+    void createRelationPermFunc(int idFunc, int idPerm);
 }
--- src/main/java/app/naturalis/backend/service/FuncionarioService.java
+++ src/main/java/app/naturalis/backend/service/FuncionarioService.java
@@ -3,9 +3,15 @@ package app.naturalis.backend.service;
 import app.naturalis.backend.model.Funcionario;
 import app.naturalis.backend.repository.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class FuncionarioService {
@@ -16,14 +22,47 @@ public class FuncionarioService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @PersistenceContext
+    private EntityManager manager;
+
 
     public Funcionario newFunc(Funcionario funcionario){
         String bcryptPass = passwordEncoder .encode(funcionario.getSenha());
         funcionario.setSenha(bcryptPass);
-        return this.funcRepo.save(funcionario);
+        Funcionario funcionarioSave = this.funcRepo.save(funcionario);
+
+        List<Integer> roles = identifPermissionsNewFunc(funcionario.getCargo().getId());
+        for (int i : roles){
+            funcRepo.createRelationPermFunc(funcionarioSave.getId(), i);
+        }
+        return funcionario;
     }
 
     public void removeFuncById(int id){
         this.funcRepo.deleteById(id);
+    }
+
+    public List<Integer> identifPermissionsNewFunc(int cargo){
+        List<Integer> permissions = new ArrayList<Integer>();
+        switch(cargo) {
+            case 1: //ADM
+                Integer permAdm[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+                permissions.addAll(Arrays.asList(permAdm));
+                break;
+            case 2: //ATENDENTE
+                Integer permAtend[] = {1,2,6,9,14};
+                permissions.addAll(Arrays.asList(permAtend));
+                break;
+            case 3: // ENTREGADOR
+                Integer permEnt[] = {12};
+                permissions.addAll(Arrays.asList(permEnt));
+                break;
+            case 4: // FARMACEUTICO
+                Integer permFarm[] = {2,4,5,6,7,9,10,11,12};
+                permissions.addAll(Arrays.asList(permFarm));
+                break;
+        }
+
+        return permissions;
     }
 }
